341bf468c469fa1ba516dc221c7acd0a3fe552bf
Kapil parmar <kapilparmar9399@gmail.com>
2026-09-22 10:16:32 -0700

    Add HackerRank Medium - No Idea!

---
+++ HackerRank/Medium/No Idea!/solution.py
@@ -0,0 +1,21 @@
+# HackerRank Problem: No Idea!
+# Link: https://www.hackerrank.com/challenges/no-idea/problem
+# Difficulty: Medium
+# Language: python3
+
+n, m = map(int, input().split())
+
+array = list(map(int, input().split()))
+
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+happiness = 0
+
+for i in array:
+    if i in A:
+        happiness += 1
+    elif i in B:
+        happiness -= 1
+
+print(happiness)
