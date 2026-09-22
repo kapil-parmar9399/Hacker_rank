bd33e051ecef631f14088db249a32ce5cd4d5697
Kapil parmar <kapilparmar9399@gmail.com>
2026-09-22 10:20:24 -0700

    Add HackerRank Easy - Say "Hello, World!" With Python

---
+++ HackerRank/Easy/Say Hello, World! With Python/solution.py
@@ -0,0 +1,7 @@
+# HackerRank Problem: Say "Hello, World!" With Python
+# Link: https://www.hackerrank.com/challenges/py-hello-world/problem
+# Difficulty: Easy
+# Language: python3
+
+my_name = "Hello, World!"
+print(my_name)
