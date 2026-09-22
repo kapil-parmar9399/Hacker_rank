e6913e680258c241512491622d4f7bd78664f037
Kapil parmar <kapilparmar9399@gmail.com>
2026-09-22 11:03:41 -0700

    Add HackerRank Easy - Integers Come In All Sizes

---
+++ HackerRank/Easy/Integers Come In All Sizes/solution.py
@@ -0,0 +1,12 @@
+# HackerRank Problem: Integers Come In All Sizes
+# Link: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+# Difficulty: Easy
+# Language: python3
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT-
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a**b + c**d)
