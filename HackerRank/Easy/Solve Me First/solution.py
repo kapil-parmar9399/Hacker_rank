ca7f339bfd4070729b1ced00b97b5c359a97c0ea
Kapil parmar <kapilparmar9399@gmail.com>
2026-09-22 10:18:55 -0700

    Add HackerRank Easy - Solve Me First

---
+++ HackerRank/Easy/Solve Me First/solution.py
@@ -0,0 +1,12 @@
+# HackerRank Problem: Solve Me First
+# Link: https://www.hackerrank.com/challenges/solve-me-first/problem
+# Difficulty: Easy
+# Language: python3
+
+def add(a, b):
+    return a + b
+
+a = int(input())
+b = int(input())
+
+print(add(a, b))
