a1047e54c77a25449a55f7eeac9438bca62de338
Kapil parmar <kapilparmar9399@gmail.com>
2026-09-22 10:19:48 -0700

    Add HackerRank Easy - Print Function

---
+++ HackerRank/Easy/Print Function/solution.py
@@ -0,0 +1,9 @@
+# HackerRank Problem: Print Function
+# Link: https://www.hackerrank.com/challenges/python-print/problem
+# Difficulty: Easy
+# Language: python3
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(1,n+1):
+    print(i, end="")    
