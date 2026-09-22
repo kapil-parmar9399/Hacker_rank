cbb45b14f462dd87648b453e052bca6c621e4cbb
Kapil parmar <kapilparmar9399@gmail.com>
2026-09-22 10:53:19 -0700

    Add HackerRank Easy - Mod Divmod

---
+++ HackerRank/Easy/Mod Divmod/solution.py
@@ -0,0 +1,8 @@
+# HackerRank Problem: Mod Divmod
+# Link: https://www.hackerrank.com/challenges/python-mod-divmod/problem
+# Difficulty: Easy
+# Language: python3
+
+
+a, b = int(input()), int(input())
+print(a // b); print(a % b); print(divmod(a, b))
