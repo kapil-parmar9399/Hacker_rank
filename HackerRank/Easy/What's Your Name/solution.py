39d4a0a4eb3f7680464f1f77db9eb616b1294417
Kapil parmar <kapilparmar9399@gmail.com>
2026-09-22 10:17:08 -0700

    Add HackerRank Easy - What's Your Name?

---
+++ HackerRank/Easy/What's Your Name/solution.py
@@ -0,0 +1,20 @@
+# HackerRank Problem: What's Your Name?
+# Link: https://www.hackerrank.com/challenges/whats-your-name/problem
+# Difficulty: Easy
+# Language: python3
+
+# first_name = input()
+# last_name = input()
+
+# print(f"Hello {first_name} {last_name}! You just delved into python")
+
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    print("Hello " + first + " " + last + "! You just delved into python.")
+
