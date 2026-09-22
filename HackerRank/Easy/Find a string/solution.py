316eb31f9c421833413bd3697fee14250445bd23
Kapil parmar <kapilparmar9399@gmail.com>
2026-09-22 10:17:02 -0700

    Add HackerRank Easy - Find a string

---
+++ HackerRank/Easy/Find a string/solution.py
@@ -0,0 +1,13 @@
+# HackerRank Problem: Find a string
+# Link: https://www.hackerrank.com/challenges/find-a-string/problem
+# Difficulty: Easy
+# Language: python3
+
+def count_substring(string, sub_string):
+    count = 0
+
+    for i in range(len(string) - len(sub_string) + 1):
+        if string[i:i + len(sub_string)] == sub_string:
+            count += 1
+
+    return count
